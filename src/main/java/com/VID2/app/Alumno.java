50fcaa6e14c60234c932cc256619c528c437763a
Ignacio <Ignacio@VIEB.iptv.microsoft.com>
2017-04-03 17:45:55 -0300

    Tercer Vídeo

---
+++ src/main/java/com/VID2/app/Alumno.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.VID2.app;
+
+/**
+ *
+ * @author Ignacio
+ */
+public class Alumno extends Persona{
+    public static int sumar (int n){
+        return n+2;
+    }
+}
