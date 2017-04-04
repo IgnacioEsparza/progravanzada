3e6d9f21cb0ad6319a74b12a4db4ca7197559c82
Ignacio <Ignacio@VIEB.iptv.microsoft.com>
2017-04-04 10:38:32 -0300

    Video 9 Capacidad Inicial ArrayList

---
+++ src/main/java/com/VID9/app/MainV9.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.VID9.app;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author Ignacio
+ */
+public class MainV9 {
+    public static void main(String[] args) {
+        List <String> lista = new LinkedList<String>();
+        System.out.println(lista.toString());
+    }
+}
